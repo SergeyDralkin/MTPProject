1e241484a6a15ecd47058602deb525b1e60aeca8
WarPackSega67 <48647638+WarPackSega66@users.noreply.github.com>
2019-12-17 11:46:26 +0300

    Update MainForm.Designer.cs

--- VideoPlayer/WindowsApplication/MainForm.Designer.cs
+++ VideoPlayer/WindowsApplication/MainForm.Designer.cs
@@ -263,9 +263,9 @@ namespace WindowsApplication
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.Controls.Add(this.Track_AudioTrack);
-            this.panel1.Location = new System.Drawing.Point(110, 406);
+            this.panel1.Location = new System.Drawing.Point(110, 402);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(305, 30);
+            this.panel1.Size = new System.Drawing.Size(305, 17);
             this.panel1.TabIndex = 0;
             // 
             // panel2
